4bf24d2f8b5b43bda87d59f2e0676e386c1aa61a
BeaAlencar <beatrizfalencar@gmail.com>
2014-11-03 10:45:38 -0400

    Update da vida

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+
+
+namespace Teste
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string arquivoSelecionado="";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+
+            dlg.InitialDirectory = "c:\\";
+
+            dlg.Filter = "Image files (*.jpg)|*.jpg|All Files (*.*)|*.*";
+
+            dlg.RestoreDirectory = true;
+
+            if (dlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+
+                arquivoSelecionado = dlg.FileName;
+                lblNomeDoArquivo.Content = arquivoSelecionado;
+                BitmapImage bitmap = new BitmapImage();
+                bitmap.BeginInit();
+                bitmap.UriSource = new Uri(arquivoSelecionado);
+                bitmap.EndInit();
+                ImageViewer.Source = bitmap;
+
+            }
+
+
+        }
+
+
+    }       
+ }
+
